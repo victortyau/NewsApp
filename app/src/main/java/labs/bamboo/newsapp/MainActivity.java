f2f9d4daafa04dc4e757d0725567a0050632c05a
Victor Tejada Yau <victortyau@gmail.com>
2021-04-07 00:15:09 -0500

    working on some details and intent to show new in browser

--- app/src/main/java/labs/bamboo/newsapp/MainActivity.java
+++ app/src/main/java/labs/bamboo/newsapp/MainActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -29,17 +33,27 @@ public class MainActivity extends AppCompatActivity {
             arrayList = new GetNews().execute("https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test").get();
 
             if ( arrayList != null ) {
-                for( int i = 0; i < arrayList.size(); i++ ) {
-                    String[] currentNews = arrayList.get(i);
-                    arrayListNews.add(new NewsView(R.drawable.jest, currentNews[0], getResources().getString(R.string.guardian)));
-                    NewsViewAdapter newsViewAdapter= new NewsViewAdapter(this, arrayListNews);
-                    newsListView.setAdapter(newsViewAdapter);
-                }
 
-                newsListView.setOnItemClickListener((parent, view, position, id) -> {
-                    intent.setData(Uri.parse(arrayList.get(position)[1]));
-                    startActivity(intent);
-                });
+                if ( arrayList.size() > 0 ) {
+                    for( int i = 0; i < arrayList.size(); i++ ) {
+                        String[] currentNews = arrayList.get(i);
+                        arrayListNews.add(new NewsView(R.drawable.jest, currentNews[0], getResources().getString(R.string.guardian)));
+                        NewsViewAdapter newsViewAdapter= new NewsViewAdapter(this, arrayListNews);
+                        newsListView.setAdapter(newsViewAdapter);
+                    }
+
+                    newsListView.setOnItemClickListener((parent, view, position, id) -> {
+                        intent.setData(Uri.parse(arrayList.get(position)[1]));
+                        startActivity(intent);
+                    });
+                }else {
+                    RelativeLayout mainActivity = findViewById(R.id.mainActivity);
+                    TextView textView = new TextView(this);
+                    textView.setText(getResources().getString(R.string.no_news));
+                    textView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    textView.setGravity(Gravity.CENTER);
+                    mainActivity.addView(textView);
+                }
             }
         }catch (ExecutionException | InterruptedException ei){
             ei.printStackTrace();
